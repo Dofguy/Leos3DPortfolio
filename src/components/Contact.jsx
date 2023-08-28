e4fded850adfb1f1dd630c2a6576a41ab5ff1e4e
Leonardo Da Silva <l.ddsilva@outlook.com>
2023-08-28 12:49:08 +0100

    Changes to text on contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -83,7 +83,7 @@ const Contact = () => {
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your name</span>
+            <span className="text-white font-medium mb-4">Full Name</span>
             <input 
               type="text"
               name="name"
@@ -94,7 +94,7 @@ const Contact = () => {
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your email</span>
+            <span className="text-white font-medium mb-4">Email</span>
             <input 
               type="email"
               name="email"
@@ -105,13 +105,13 @@ const Contact = () => {
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your message</span>
+            <span className="text-white font-medium mb-4">Message</span>
             <textarea
               rows="7"
               name="message"
               value={form.message}
               onChange={handleChange}
-              placeholder="What do you want to say?"
+              placeholder="Leave a message!"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
@@ -120,7 +120,7 @@ const Contact = () => {
             type="submit"
             className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
           >
-            {loading ? 'sending...' : 'send'}
+            {loading ? 'Sending...' : 'Send'}
           </button>
         </form>
       </motion.div>
